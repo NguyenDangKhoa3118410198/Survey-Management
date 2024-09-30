6b88dbcc70ec6ca7df340405e62518361ffd5276
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2024-09-30 21:53:16 +0700

    change font weight for input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,100 +1,102 @@
-import { Button, Flex, Form, Input, Typography } from "antd";
-import useStore from "../hooks/useStore";
+import { Button, Flex, Form, Input, Typography } from 'antd';
+import useStore from '../hooks/useStore';
 
 export default function Login() {
-  const { setUser } = useStore();
-  const onSubmit = (values) => {
-    const { email, password } = values;
-    const date = Date.now();
-    const token = btoa(email, password, date);
-    setUser(email, token);
-  };
-  return (
-    <Flex justify='center' align='center' style={{ height: "100%" }}>
-      <Form
-        onFinish={onSubmit}
-        autoComplete='off'
-        style={{ width: "350px", margin: "auto" }}
-      >
-        <Typography.Title
-          level={2}
-          style={{
-            textAlign: "center",
-            fontWeight: 700,
-            color: "var(--main-color)",
-          }}
-        >
-          Admin Panel
-        </Typography.Title>
-        <Form.Item
-          name='email'
-          rules={[
-            {
-              required: true,
-              message: "Vui lòng nhập email",
-            },
-            {
-              pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: "Email không hợp lệ",
-            },
-          ]}
-        >
-          <Input
-            placeholder='Email'
-            style={{
-              width: "100%",
-              border: "4px solid var(--main-color)",
-              padding: "8px 11px",
-              borderRadius: "14px",
-              color: "var(--main-color)",
-              fontSize: "16px",
-              fontWeight: 800,
-            }}
-          />
-        </Form.Item>
+   const { setUser } = useStore();
+   const onSubmit = (values) => {
+      const { email, password } = values;
+      const date = Date.now();
+      const token = btoa(email, password, date);
+      setUser(email, token);
+   };
+   return (
+      <Flex justify='center' align='center' style={{ height: '100%' }}>
+         <Form
+            onFinish={onSubmit}
+            autoComplete='off'
+            style={{ width: '350px', margin: 'auto' }}
+         >
+            <Typography.Title
+               level={2}
+               style={{
+                  textAlign: 'center',
+                  fontWeight: 700,
+                  color: 'var(--main-color)',
+               }}
+            >
+               Admin Panel
+            </Typography.Title>
+            <Form.Item
+               name='email'
+               rules={[
+                  {
+                     required: true,
+                     message: 'Vui lòng nhập email',
+                  },
+                  {
+                     pattern:
+                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                     message: 'Email không hợp lệ',
+                  },
+               ]}
+            >
+               <Input
+                  placeholder='Email'
+                  style={{
+                     width: '100%',
+                     border: '3px solid var(--main-color)',
+                     padding: '8px 11px',
+                     borderRadius: '14px',
+                     color: 'var(--main-color)',
+                     fontSize: '16px',
+                     fontWeight: 600,
+                  }}
+               />
+            </Form.Item>
 
-        <Form.Item
-          name='password'
-          rules={[
-            { required: true, message: "Vui lòng nhập mật khẩu" },
-            {
-              pattern: /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
-              message:
-                "Mật khẩu đảm bảo có ít nhất 8 ký tự, ít nhất 1 ký tự chữ và ít nhất 1 ký tự đặc biệt",
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder='Vui lòng nhập mật khẩu'
-            style={{
-              width: "100%",
-              border: "4px solid var(--main-color)",
-              padding: "8px 11px",
-              borderRadius: "14px",
-              color: "var(--main-color)",
-              fontSize: "16px",
-              fontWeight: 800,
-            }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type='primary'
-            htmlType='submit'
-            style={{
-              width: "100%",
-              backgroundColor: "var(--main-color)",
-              height: "49px",
-              borderRadius: "14px",
-              color: "white",
-              fontSize: "16px",
-              fontWeight: 800,
-            }}
-          >
-            Đăng nhập
-          </Button>
-        </Form.Item>
-      </Form>
-    </Flex>
-  );
+            <Form.Item
+               name='password'
+               rules={[
+                  { required: true, message: 'Vui lòng nhập mật khẩu' },
+                  {
+                     pattern:
+                        /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
+                     message:
+                        'Mật khẩu đảm bảo có ít nhất 8 ký tự, ít nhất 1 ký tự chữ và ít nhất 1 ký tự đặc biệt',
+                  },
+               ]}
+            >
+               <Input.Password
+                  placeholder='Vui lòng nhập mật khẩu'
+                  style={{
+                     width: '100%',
+                     border: '3px solid var(--main-color)',
+                     padding: '8px 11px',
+                     borderRadius: '14px',
+                     color: 'var(--main-color)',
+                     fontSize: '16px',
+                     fontWeight: 600,
+                  }}
+               />
+            </Form.Item>
+            <Form.Item>
+               <Button
+                  type='primary'
+                  htmlType='submit'
+                  style={{
+                     width: '100%',
+                     backgroundColor: 'var(--main-color)',
+                     height: '49px',
+                     borderRadius: '14px',
+                     color: 'white',
+                     fontSize: '16px',
+                     fontWeight: 700,
+                  }}
+               >
+                  Đăng nhập
+               </Button>
+            </Form.Item>
+         </Form>
+      </Flex>
+   );
 }
