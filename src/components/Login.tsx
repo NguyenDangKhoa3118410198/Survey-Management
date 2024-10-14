f2aa58d8c4c2811081b2e8f0eba9fe354cc5aa5f
Dang Khoa <ndk11082@gmail.com>
2024-10-14 09:46:21 +0700

    restyle login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,7 @@ import { Button, Checkbox, Flex, Form, Input, Typography } from 'antd';
 import useStore from '../hooks/useStore';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
 
 interface LoginFormValues {
   email: string;
@@ -51,94 +52,136 @@ const Login: React.FC = () => {
   };
 
   return (
-    <Flex justify='center' align='center' style={{ height: '100%' }}>
+    <Flex
+      justify='center'
+      align='center'
+      style={{ height: '100%', position: 'relative' }}
+    >
       <Form
         form={form}
         onFinish={onSubmit}
         onKeyDown={handlePressEnter}
         autoComplete='off'
-        style={{ width: '350px', margin: 'auto' }}
+        style={{
+          margin: 'auto',
+          border: '2px solid var(--main-color)',
+          padding: 24,
+          display: 'flex',
+          height: '500px',
+          borderRadius: '14px',
+          boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)',
+          alignItems: 'center',
+        }}
       >
-        <Typography.Title
-          level={2}
-          style={{
-            textAlign: 'center',
-            fontWeight: 700,
-            color: 'var(--main-color)',
-            marginBottom: '30px',
-          }}
-        >
-          Admin Panel Management Survey
-        </Typography.Title>
-        <Form.Item
-          name='email'
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng nhập email',
-            },
-            {
-              pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: 'Email không hợp lệ',
-            },
-          ]}
-        >
-          <Input
-            placeholder='Email'
+        <img
+          src='/imgs/survey.jpg'
+          alt='error image'
+          style={{ width: '350px', marginRight: '50px' }}
+        />
+        <Flex vertical>
+          <Typography.Title
+            level={4}
             style={{
-              border: '3px solid var(--main-color)',
-              padding: '8px 11px',
-              color: 'var(--main-color)',
-            }}
-          />
-        </Form.Item>
-
-        <Form.Item
-          name='password'
-          rules={[
-            { required: true, message: 'Vui lòng nhập mật khẩu' },
-            {
-              pattern: /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
-              message:
-                'Mật khẩu đảm bảo có ít nhất 8 ký tự, ít nhất 1 ký tự chữ và ít nhất 1 ký tự đặc biệt',
-            },
-          ]}
-          style={{ marginBottom: '5px' }}
-        >
-          <Input.Password
-            placeholder='Vui lòng nhập mật khẩu'
-            style={{
-              border: '3px solid var(--main-color)',
-              padding: '8px 11px',
-              color: 'var(--main-color)',
-            }}
-          />
-        </Form.Item>
-        <Form.Item
-          name='remember'
-          valuePropName='checked'
-          style={{ margin: '10px 0' }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type='primary'
-            htmlType='submit'
-            style={{
-              width: '100%',
-              backgroundColor: 'var(--main-color)',
-              height: '49px',
-              color: 'white',
               fontWeight: 700,
-              marginTop: '10px',
+              color: 'var(--main-color)',
+              marginBottom: '30px',
             }}
           >
-            Đăng nhập
-          </Button>
-        </Form.Item>
+            <h1
+              style={{
+                textAlign: 'left',
+                fontWeight: 700,
+                color: 'var(--main-color)',
+              }}
+            >
+              The Liems
+            </h1>
+            Admin Panel Management Survey
+          </Typography.Title>
+          <Form.Item
+            name='email'
+            rules={[
+              {
+                required: true,
+                message: 'Vui lòng nhập email',
+              },
+              {
+                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: 'Email không hợp lệ',
+              },
+            ]}
+          >
+            <Input
+              prefix={<MailOutlined />}
+              placeholder='Email'
+              style={{
+                border: '3px solid var(--main-color)',
+                padding: '8px 11px',
+                color: 'var(--main-color)',
+                borderRadius: '14px',
+              }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name='password'
+            rules={[
+              { required: true, message: 'Vui lòng nhập mật khẩu' },
+              {
+                pattern:
+                  /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
+                message:
+                  'Mật khẩu đảm bảo có ít nhất 8 ký tự, ít nhất 1 ký tự chữ và ít nhất 1 ký tự đặc biệt',
+              },
+            ]}
+            style={{ marginBottom: '5px' }}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder='Vui lòng nhập mật khẩu'
+              style={{
+                border: '3px solid var(--main-color)',
+                padding: '8px 11px',
+                color: 'var(--main-color)',
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            name='remember'
+            valuePropName='checked'
+            style={{ margin: '10px 0' }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type='primary'
+              htmlType='submit'
+              style={{
+                width: '100%',
+                backgroundColor: 'var(--main-color)',
+                height: '49px',
+                color: 'white',
+                fontWeight: 700,
+                marginTop: '10px',
+              }}
+            >
+              Đăng nhập
+            </Button>
+          </Form.Item>
+        </Flex>
       </Form>
+      <p
+        style={{
+          position: 'absolute',
+          right: 0,
+          bottom: 0,
+          padding: 12,
+        }}
+      >
+        version 1.0
+      </p>
     </Flex>
   );
 };
